d51e0f182449a9c3c0f3178dbb1310773e52ab5f
Max Base <maxbasecode@gmail.com>
2023-01-14 14:37:16 +0100

    create function

---
+++ 0-1Knapsack.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int **dp = (int **) malloc((n + 1) * sizeof(int *));
+    for (int i = 0; i <= n; i++) {
+        dp[i] = (int *) malloc((W + 1) * sizeof(int));
+    }
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (i == 0 || w == 0) {
+                dp[i][w] = 0;
+            } else if (wt[i-1] <= w) {
+                dp[i][w] = MAX(val[i-1] + dp[i-1][w-wt[i-1]], dp[i-1][w]);
+            } else {
+                dp[i][w] = dp[i-1][w];
+            }
+        }
+    }
+
+    int result = dp[n][W];
+
+    for (int i = 0; i <= n; i++) {
+        free(dp[i]);
+    }
+    free(dp);
+
+    return result;
+}
